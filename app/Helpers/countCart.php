3b01ae6102effa40b2a53eb38ade8209bd0e14df
DuongDuc <duc.dd108@gmail.com>
2020-11-30 12:52:34 +0700

    make Helper to show count cart

---
+++ app/Helpers/countCart.php
@@ -0,0 +1,15 @@
+
+<?php
+use Illuminate\Support\Facades\Session;
+if (!function_exists('countCart')) {
+
+    function countCart($name)
+    {
+       $cart = Session::get($name) ?? collect();
+       $total = 0;
+       foreach ($cart as $product){
+           $total += $product->quantity;
+       }
+       return $total;
+    }
+}
